7ee998e6db797d7f82106d2e1839c7a8cab50322
Dhruvi lakhani <lakhanidhruvi3@gmail.com>
2026-06-18 15:39:04 +0530

    Add files via upload

---
+++ RX_code.ino
@@ -0,0 +1,71 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <SPI.h>
+#include <LoRa.h>
+
+#define SS 5
+#define RST 14
+#define DIO0 26
+
+const char* ssid = "Krishna";
+const char* password = "Kri20412";
+
+#define BOT_TOKEN "8737376101:AAEdBf9LfR1q-DGuZvE6JaI3QzJTJ5tenrg"
+#define CHAT_ID "6770284216"
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.begin(ssid,password);
+
+  Serial.print("Connecting WiFi");
+
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  client.setInsecure();
+
+  Serial.println("\nWiFi Connected");
+
+  LoRa.setPins(SS,RST,DIO0);
+
+  if(!LoRa.begin(433E6))
+  {
+    Serial.println("LoRa init failed");
+    while(1);
+  }
+
+  Serial.println("LoRa Receiver Ready");
+}
+
+void loop()
+{
+  int packetSize = LoRa.parsePacket();
+
+  if(packetSize)
+  {
+    String data="";
+
+    while(LoRa.available())
+    {
+      data += (char)LoRa.read();
+    }
+
+    Serial.println("SOS LOCATION RECEIVED:");
+    Serial.println(data);
+
+    String message = "🚨 SOS ALERT!\n\nBlind person needs help.\n\nLocation:\nhttps://maps.google.com/?q=" + data;
+
+    bot.sendMessage(CHAT_ID, message, "");
+
+    Serial.println("Location sent to phone");
+  }
+}
